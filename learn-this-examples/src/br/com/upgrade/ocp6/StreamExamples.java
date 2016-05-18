f6bcf7be7724ad4ec8ef791e68359f3918d52e97
ggarcia <ggarcia.java@gmail.com>
2016-05-17 23:08:31 -0300

    Exemplos de Stream com Array

--- learn-this-examples/src/br/com/upgrade/ocp6/StreamExamples.java
+++ learn-this-examples/src/br/com/upgrade/ocp6/StreamExamples.java
@@ -1,11 +1,15 @@
 package br.com.upgrade.ocp6;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class StreamExamples {
 
 	public static void main(String[] args) {
 		exampleIntStream();
+		exampleArrayStream();
 	}
 	
 	/**
@@ -24,6 +28,29 @@ public class StreamExamples {
 		
 	}
 	
+	/**
+	 * Exemplo de uso da API de Stream com Array
+	 */
+	public static void exampleArrayStream() {
+		
+		List<String> myList = Arrays.asList("item 11", "item 12", "item 31", "item 35", "item 45");
+		System.out.println("");
+		
+		myList.stream()
+			.filter(s -> s.contains("5"))
+			.map(String::toUpperCase)
+			.forEach(s -> System.out.print(s +", "));
+		
+		System.out.println("");
+		List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+		
+		//filtra uma lista de quadrados sem repetição
+		numbers.stream()
+			.map( i -> i*i)
+			.distinct()
+			.forEach(s -> System.out.print(s + ", "));
+		
+	}
 	
 	
 }
