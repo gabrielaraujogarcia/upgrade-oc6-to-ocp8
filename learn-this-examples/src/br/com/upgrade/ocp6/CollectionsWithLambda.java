d55f4e261d83999763bc5a575e44025d8dfb5919
Gabriel Araújo Garcia <ggarcia.java@gmail.com>
2016-05-30 23:27:32 -0300

    Adicionado exemplos de operações lambda com stream e collection

--- learn-this-examples/src/br/com/upgrade/ocp6/CollectionsWithLambda.java
+++ learn-this-examples/src/br/com/upgrade/ocp6/CollectionsWithLambda.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CollectionsWithLambda {
 	
@@ -16,56 +17,39 @@ public class CollectionsWithLambda {
 	};
 
 	public static void main(String[] args) {
+		iteratesFilterAndSort();
+		searchData();
+		doCalculations();
+	}
+	
+	/**
+	 * Develop code that iterates a collection, filters a collection, and sorts a 
+	 * collection by using lambda expressions
+	 */
+	private static void iteratesFilterAndSort() {
 		iterates();
 		filter();
 		sort();
-		findFirst();
-		findAny();
-		anyMatch();
-		allMatch();
-		noneMatch();
-		sum();
-		
-//		Runnable runnable = new Runnable() {
-//			
-//			@Override
-//			public void run() {
-//				findAny();
-//			}
-//			
-//		};
-		
-//		int i = 0;
-//		while(i < 5) {
-//			new Thread(runnable).start();
-//			new Thread(runnable).start();
-//			new Thread(runnable).start();
-//			i++;
-//		}
-		
+		System.out.println("");
 	}
 	
 	/**
 	 * Percorre a lista de jogadores e então soma a idade de todos eles 
 	 */
 	private static void iterates() {
-		
 		list.stream()
 			.forEach(p -> sumOfAges += p.getAge());
 		System.out.println("Sum of ages: "+ sumOfAges);
-		System.out.println("");
 	}
 	
 	/**
 	 * Filtra a coleção e exibe o nome dos jogadores com mais de 25 anos
 	 */
 	private static void filter() {
-		
 		list.stream() //retorna um Stream da coleção  
 			.filter(p -> p.getAge() > 25) //filtra os jogadores com idade maior que 25 anos
 			.forEach(p -> System.out.println(p.getFirstName())); //percorre cada elemento filtrado e escreve no console
 		System.out.println("");
-		
 	}
 	
 	/**
@@ -92,8 +76,20 @@ public class CollectionsWithLambda {
 		Collections.sort(list, CollectionsWithLambda::orderByLastName);
 		list.stream().forEach(p -> System.out.println(p.getFirstName() + " " +p.getLastName()
 			+" " +p.getAge()));
-		System.out.println("");
 		
+	}
+	
+	/**
+	 * Tópico: Search for data by using methods, such as findFirst(), findAny(), anyMatch(), 
+	 * allMatch(), and noneMatch()
+	 */
+	private static void searchData() {
+		findFirst(); 	//primeira ocorrÊncia
+		findAny(); 		//alguma ocorrência
+		anyMatch(); 	//qualquer ocorrência
+		allMatch(); 	//todas as ocorrências
+		noneMatch(); 	//nenhuma ocorrência
+		System.out.println("");
 	}
 	
 	/**
@@ -107,7 +103,6 @@ public class CollectionsWithLambda {
 		
 		//imprime somente se for encontrado
 		findFirst.ifPresent(p -> System.out.println("First finded: " + p.getFirstName()));
-		System.out.println("");
 	}
 	
 	/**
@@ -125,7 +120,6 @@ public class CollectionsWithLambda {
 				.filter(p2 -> p2.getAge() > 28 )
 				.findAny()
 				.ifPresent(p3 -> System.out.println("One more time: " +p3.getFirstName()));
-		System.out.println("");
 		
 	}
 	
@@ -137,7 +131,6 @@ public class CollectionsWithLambda {
 		
 		Boolean moreThen31 = list.stream().anyMatch(p -> p.getAge() > 31);
 		System.out.println("Someone here have more then 31 age? " + moreThen31);
-		System.out.println("");
 		
 	}
 	
@@ -149,7 +142,6 @@ public class CollectionsWithLambda {
 		
 		boolean moreThen23 = list.stream().allMatch(p -> p.getAge() > 23);
 		System.out.println("All players have more then 23 age? " + moreThen23);
-		System.out.println("");
 		
 	}
 
@@ -164,16 +156,85 @@ public class CollectionsWithLambda {
 	}
 	
 	/*
-	 * Perform calculations on Java Streams by using count, max, min, average, and sum methods 
-	 * and save results to a collection by using the collect method and Collector class, including 
-	 * the averagingDouble, groupingBy, joining, partitioningBy methods
+	 * 
 	 */
 	
 	/**
+	 * Tópico: Perform calculations on Java Streams by using count, max, min, average and sum  
+	 * methods and save results to a collection by using the collect method and Collector class,
+	 * including the averagingDouble, groupingBy, joining, partitioningBy methods 
+	 */
+	private static void doCalculations() {
+		count();
+		max();
+		min();
+		average();
+		sum();
+		reduce(); //bonus
+		System.out.println("");
+	}
+	
+	/**
+	 * Conta quantos elementos possui no Stream da coleção
+	 */
+	private static void count() {
+		System.out.println("Count a new ArrayList: " + new ArrayList<>().stream().count());
+		System.out.println("Count a list of person: " + list.stream().count());
+	}
+	
+	/**
+	 * Mapeia o Stream de pessoas pela idade e então retorna a pessoa mais velha
+	 */
+	private static void max() {
+		list.stream()
+			.map(person -> person.getAge()) //ou Person::getAge
+			.max((a1, a2) -> a1.compareTo(a2))
+			.ifPresent(maxAge -> System.out.println("Older person: " + maxAge));
+	}
+	
+	/**
+	 * Mapeia o Stream de pessoas pelo ultimo nome e então retorna o menor nome da ordem
+	 * crescente do alfabeto
+	 */
+	private static void min() {
+		//Comparator<Person> comp = (p1, p2) -> p1.getLastName().compareTo(p2.getLastName());
+		list.stream()
+			.map(Person::getLastName)
+			.min((p1, p2) -> p1.compareTo(p2)) //ou trocar por comp e remover a linha anterior
+			.ifPresent(s -> System.out.println("Min last name: " + s));
+	}
+	
+	/**
+	 * Calcula a média das idades na lista
+	 */
+	private static void average() {
+		list.stream()
+			.mapToInt(Person::getAge)
+			.average()
+			.ifPresent(age -> System.out.println("Average: " + age));
+	}
+	
+	/**
+	 * Maneiras de realizar uma soma na coleção com Java 8 e lambda
 	 */
 	private static void sum() {
+	
+		//Soma a idade das pessoas utilizando o método Collectors.summingInt(Function<? super T>
+		Integer sum2 = list.stream().collect(Collectors.summingInt(Person::getAge));
+		System.out.println("Sum of ages with Collectors: " + sum2);
 		
-		//reduz o Stream<Integer>, somando cada elemento e retorna um Optional<Integer>
+		//transforma o stream em um Stream de inteiros e então realiza a soma
+		int other = list.stream().mapToInt(Person::getAge).sum();
+		System.out.println("Sum of ages (better way): "+other);
+		
+	}
+	
+	/**
+	 * Exemplos que utilizam a API reduce do Stream<T> 
+	 */
+	private static void reduce() {
+		
+		//reduz em um Stream<Integer>, somando cada elemento e retorna um Optional<Integer>
 		Optional<Integer> sum = list.stream()
 			.map(Person::getAge) 
 			.reduce((a, b) -> a + b);
@@ -185,12 +246,6 @@ public class CollectionsWithLambda {
 			.map(Person::getAge) //retorna um Stream<Integer>
 			.reduce(6, (a, b) -> a + b);
 		System.out.println("Sum of ages + 6: " + sumPlus6);
-		
-		//transforma o stream em um Stream de inteiros e então realiza a soma
-		int betterWay = list.stream()
-			.mapToInt(Person::getAge)
-			.sum();
-		System.out.println("Sum of ages (better way): "+betterWay);
 		
 	}
 	
